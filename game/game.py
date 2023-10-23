29bcbdaad03a4185f1d3c246f55af3de1e8b87bb
maxshchurr <mxm.shchur@gmail.com>
2023-10-24 01:09:09 +0300

    code refactoring; extended player class with throw_dices() method;

--- game/game.py
+++ game/game.py
@@ -2,21 +2,49 @@ from game_config import quantity_of_bots, quantity_of_dices
 from player.player import Player, HumanPlayer
 
 
-def create_human_player():
-    name = 'Human Player'
-    human = HumanPlayer(name=name, dices=quantity_of_dices)
-    return human
+class Game:
+    def __init__(self, quantity_of_bots: int, dices: int):
+        self._players = []
+        self._quantity_of_bots = quantity_of_bots
+        self._quantity_of_dices = dices
 
+    def create_human_player(self, name):
+        name = 'Human Player'
+        human = HumanPlayer(name=name, dices=self._quantity_of_dices)
+        self._players.append(human)
 
-def create_bot_players(quantity_of_bots):
-    created_bots = []
-    for i in range(quantity_of_bots):
-        new_bot = Player(dices=quantity_of_dices)
-        created_bots.append(new_bot)
+    def create_bot_players(self):
+        for i in range(self._quantity_of_bots):
+            new_bot = Player(dices=self._quantity_of_dices)
+            self._players.append(new_bot)
 
-    return created_bots
+    def get_players(self):
+        return [player for player in self._players]
+
+    def get_players_names(self):
+        return [player.get_player_name() for player in self._players]
+
+    # maybe store the result in a class variable?
+    def count_face_values_on_dices_for_all_players(self) -> dict:
+        face_values_on_dices_for_all_players = {face_value: 0 for face_value in range(1, 7)}
+
+        for player in self._players:
+            players_hand = player.get_current_hand()
+            for k in players_hand.keys():
+                face_values_on_dices_for_all_players[k] += players_hand[k]
+
+        return face_values_on_dices_for_all_players
 
 
 if __name__ == "__main__":
-    player = create_human_player()
-    bots = create_bot_players(quantity_of_bots)
+    game = Game(quantity_of_bots, quantity_of_dices)
+
+    game.create_bot_players()
+    game.create_human_player('Max')
+    players = game.get_players()
+
+    for player in players:
+        player.throw_dices()
+        print(player.get_player_name(), player.get_current_hand())
+
+    print(game.count_face_values_on_dices_for_all_players())
--- player/player.py
+++ player/player.py
@@ -1,4 +1,5 @@
 from faker import Faker
+import random
 
 
 class Player:
@@ -6,8 +7,24 @@ class Player:
 
     def __init__(self, **kwargs):
         fake = Faker()
-        self.name = kwargs.get('name', fake.name())
-        self.dices = kwargs.get('dices', 5)
+        self._name = kwargs.get('name', fake.name())
+        self._dices = kwargs.get('dices', 5)
+        self._current_hand = {}
+
+    def throw_dices(self):
+        dices = [random.randint(1, 6) for dice in range(self._dices)]
+
+        for face_value in range(1, 7):
+            if face_value not in dices:
+                self._current_hand[face_value] = 0
+            else:
+                self._current_hand[face_value] = dices.count(face_value)
+
+    def get_current_hand(self) -> dict:
+        return self._current_hand
+
+    def get_player_name(self):
+        return self._name
 
 
 class HumanPlayer(Player):
